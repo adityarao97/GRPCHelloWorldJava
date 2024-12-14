a8fee952db43955b31fc519c0706395f6ebc9a45
Aditya <adi97rao@gmail.com>
2024-12-14 15:40:41 -0800

    changes to run a server and client

--- src/main/java/org/example/grpc/HelloWorldClient.java
+++ src/main/java/org/example/grpc/HelloWorldClient.java
@@ -1,23 +1,57 @@
 package org.example.grpc;
 
-import com.example.grpc.GreeterGrpc;
-import com.example.grpc.HelloWorldProto;
+import com.example.grpc.Mini3Proto;
+import com.example.grpc.StoreServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldClient {
     public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+        ManagedChannel channel1 = ManagedChannelBuilder.forAddress("localhost", 8080)
                 .usePlaintext()
                 .build();
 
-        GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
+        ManagedChannel channel2 = ManagedChannelBuilder.forAddress("localhost", 8081)
+                .usePlaintext()
+                .build();
 
-        HelloWorldProto.HelloRequest request = HelloWorldProto.HelloRequest.newBuilder().setName("Aditya!").build();
-        HelloWorldProto.HelloReply reply = stub.sayHello(request);
+//        GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
+        StoreServiceGrpc.StoreServiceBlockingStub storeService1 = StoreServiceGrpc.newBlockingStub(channel1);
+        StoreServiceGrpc.StoreServiceBlockingStub storeService2 = StoreServiceGrpc.newBlockingStub(channel2);
 
-        System.out.println(reply.getMessage());
+//        HelloWorldProto.HelloRequest request = HelloWorldProto.HelloRequest.newBuilder().setName("Aditya!").build();
+//        HelloWorldProto.HelloReply reply = stub.sayHello(request);
 
-        channel.shutdown();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        Mini3Proto.PutMessageRequest requestPutMessage = putMapValue(map);
+        Mini3Proto.PutMessageResponse putMessageResponse = storeService1.putMessage(requestPutMessage);
+        System.out.println("map value from server 1 is " + putMessageResponse.toString());
+
+
+        HashMap<String, String> map2 = new HashMap<>();
+        map2.put("key2", "value3");
+        map2.put("key3", "value3");
+        Mini3Proto.PutMessageRequest requestPutMessage2 = putMapValue(map2);
+        Mini3Proto.PutMessageResponse putMessageResponse2 = storeService2.putMessage(requestPutMessage2);
+        System.out.println("map value from server 2 is " + putMessageResponse2.toString());
+
+        Mini3Proto.EmptyInput requestEmptyInput = Mini3Proto.EmptyInput.newBuilder().build();
+        Mini3Proto.GetKeySizeResponse getKeySizeResponse1 = storeService1.getKeySize(requestEmptyInput);
+        Mini3Proto.GetKeySizeResponse getKeySizeResponse2 = storeService2.getKeySize(requestEmptyInput);
+        System.out.println("server 1 map key size is : " + getKeySizeResponse1.getGetKeySizeResponse());
+        System.out.println("server 2 map key size is : " + getKeySizeResponse2.getGetKeySizeResponse());
+//        System.out.println(reply.getMessage());
+
+        channel1.shutdown();
+        channel2.shutdown();
+    }
+
+    private static Mini3Proto.PutMessageRequest putMapValue(HashMap<String, String> map) {
+        return Mini3Proto.PutMessageRequest.newBuilder().putAllMap(map).build();
     }
 }
--- src/main/java/org/example/grpc/HelloWorldServer.java
+++ src/main/java/org/example/grpc/HelloWorldServer.java
@@ -1,30 +1,59 @@
 package org.example.grpc;
 
 import com.example.grpc.GreeterGrpc;
-import com.example.grpc.HelloWorldProto;
+import com.example.grpc.Mini3Proto;
+import com.example.grpc.StoreServiceGrpc;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class HelloWorldServer {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(8080)
-                .addService(new GreeterImpl())
+        Server server1 = ServerBuilder.forPort(8080)
+                .addService(new StoreServiceImpl())
                 .build();
 
-        System.out.println("Server started at port 8080");
-        server.start();
-        server.awaitTermination();
+        Server server2 = ServerBuilder.forPort(8081)
+                .addService(new StoreServiceImpl())
+                .build();
+
+        System.out.println("Server1 started at port 8080");
+        System.out.println("Server2 started at port 8081");
+        server1.start();
+        server2.start();
+        server1.awaitTermination();
+        server2.awaitTermination();
     }
 
     static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
         @Override
-        public void sayHello(HelloWorldProto.HelloRequest request, StreamObserver<HelloWorldProto.HelloReply> responseObserver) {
+        public void sayHello(Mini3Proto.HelloRequest request, StreamObserver<Mini3Proto.HelloReply> responseObserver) {
             String message = "Hello, " + request.getName();
-            HelloWorldProto.HelloReply reply = HelloWorldProto.HelloReply.newBuilder().setMessage(message).build();
+            Mini3Proto.HelloReply reply = Mini3Proto.HelloReply.newBuilder().setMessage(message).build();
             responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+
+    static class StoreServiceImpl extends StoreServiceGrpc.StoreServiceImplBase {
+        HashMap<String, String> map = new HashMap<>();
+        @Override
+        public void putMessage(Mini3Proto.PutMessageRequest request, StreamObserver<Mini3Proto.PutMessageResponse> responseObserver) {
+            map.putAll(request.getDefaultInstanceForType().getMapMap());
+            Mini3Proto.PutMessageResponse response = Mini3Proto.PutMessageResponse.newBuilder().putAllPutResponse(map).build();
+            System.out.println("Server map value is : " + map.toString());
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void getKeySize(Mini3Proto.EmptyInput emptyInput, StreamObserver<Mini3Proto.GetKeySizeResponse> responseObserver) {
+            System.out.println("Key size is : " + map.size());
+            Mini3Proto.GetKeySizeResponse response = Mini3Proto.GetKeySizeResponse.newBuilder().setGetKeySizeResponse(map.size()).build();
+            responseObserver.onNext(response);
             responseObserver.onCompleted();
         }
     }
